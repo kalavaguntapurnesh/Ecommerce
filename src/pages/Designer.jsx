759c038b571eee5e9391a0b0218b8b6116cf9fb9
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-06-17 16:34:36 +0530

    Further Additions

--- src/pages/Designer.jsx
+++ src/pages/Designer.jsx
@@ -8,8 +8,195 @@ const Designer = () => {
   return (
     <div>
       <Navbar />
-      <ScrollToTop/>
-      <Sports />
+      <ScrollToTop />
+      <div className="bg-white">
+        <div className="relative pt-32 pb-8">
+          <div className="w-full">
+            <div className="w-full px-4 mx-auto max-w-[1400px]">
+              <div className="flex justify-center items-center">
+                <h1 className="font-semibold text-3xl">Select your sport</h1>
+              </div>
+
+              <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 pt-8">
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer bg-white bg-clip-border lg:h-60 rounded"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/80323881?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-20 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">Cricket</h2>
+                  </div>
+                </a>
+
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full  bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/6549938643759787?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">Football</h2>
+                  </div>
+                </a>
+
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/4576696267732981?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">Basket Ball</h2>
+                  </div>
+                </a>
+
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/5286683450111083?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">Baseball</h2>
+                  </div>
+                </a>
+
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/80323882?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">Tennis</h2>
+                  </div>
+                </a>
+
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/87713819?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">Badminton</h2>
+                  </div>
+                </a>
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/3019308249223372?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">
+                      Custom Jerseys
+                    </h2>
+                  </div>
+                </a>
+
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/8446229002489966?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">Rugby</h2>
+                  </div>
+                </a>
+
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/8606890564664600?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">
+                      Custom Jerseys
+                    </h2>
+                  </div>
+                </a>
+
+                <a
+                  href="/cricket-kit"
+                  class="relative cursor-pointer rounded bg-white bg-clip-border shadow-md lg:h-60"
+                >
+                  <div class="h-full bg-clip-border hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-1000 ease-in-out rounded">
+                    <img
+                      src="https://api.kitbuilder.co.uk/Api/file/8692078758620512?height=420&width=500&distributorId=74568098"
+                      alt="ui/ux review check"
+                      className="rounded h-[100%] w-full"
+                    />
+                  </div>
+                  <div class="absolute inset-0 bg-gray-700 opacity-30 rounded-md"></div>
+                  <div class="absolute inset-0 flex items-end mb-6 justify-center">
+                    <h2 class="text-white text-2xl font-bold">
+                      Custom Jerseys
+                    </h2>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
